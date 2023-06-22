cef82f39f4faa797250e968d75583f38ccd3ec69
Henri Seppa <henri.seppa@tuni.fi>
2023-06-22 14:49:41 +0300

    modified the way list is calculated

--- part1/number_char_pairs.hs
+++ part1/number_char_pairs.hs
@@ -8,12 +8,18 @@ charsDivisibleBy num = [numToLetter x | x <- [1..26], x `mod` num == 0]
 charsProductOf :: [Int] -> [Char] -- given a list of numbers ns, returns all the characters that have a number that is a product of any two numbers in ns. 
 -- You may assume that each number only appears in the list once.
 
-charsProductOf nums = [numToLetter x | x <- [1..26], x `elem` calculateProducts nums]
+charsProductOf nums = [numToLetter x | x <- [1..26], x `elem` calculateList nums nums]
 
-calculateProducts :: [Int] -> [Int]
-calculateProducts numbers = concat [[x * y | y <- removeOne x numbers] | x <- numbers]
-  where
-    removeOne x = filter (/= x)
+
+
+calculateList :: [Int] -> [Int] -> [Int]
+calculateList [] _ = []
+calculateList num1@(x:xs) num2@(y:ys)
+  | null xs = []
+  | x == y = calculateList (x:xs) (dropWhile (==x) ys)
+  | not (null ys) = x * y: calculateList (x:xs) ys
+  | otherwise = x * y: calculateList xs xs
+
 
 numToLetter :: Int -> Char
 
